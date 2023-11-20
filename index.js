5a1411c42cf08ad6da744ea59b9b077ce40bd532
Justin Byrd <justinbyrd7@gmail.com>
2023-11-20 15:06:16 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require("./api/server.js");
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+});
