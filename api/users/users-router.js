53c984e1e7223094ab9f0c9e048214328e4e61bf
Justin Byrd <justinbyrd7@gmail.com>
2023-11-23 12:40:08 -0800

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
-const { validateUserId } = require("../middleware/middleware.js");
+const { validateUserId, validateUser } = require("../middleware/middleware.js");
 
 const router = express.Router();
 
@@ -16,16 +16,18 @@ router.get("/:id", validateUserId, (req, res) => {
   console.log(req.user);
 });
 
-router.post("/", (req, res) => {
+router.post("/", validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+  console.log(req.name);
 });
 
-router.put("/:id", validateUserId, (req, res) => {
+router.put("/:id", validateUser, validateUserId, (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
   console.log(req.user);
+  console.log(req.name);
 });
 
 router.delete("/:id", validateUserId, (req, res) => {
